78394b08ada9af7112a46a5ade583f3906a8240a
nikosai <nikosai2531@gmail.com>
2020-04-18 19:59:58 +0900

    add browser-sync

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,27 +1,27 @@
 import express from "express";
-import { createServer } from "http";
+import * as http from "http";
 import socketio from "socket.io";
 import { Util } from "../util";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
-const server = createServer(app)
+const server = http.createServer(app)
 const io = socketio(server);
 
 app.use(express.static("docs"));
 
 app.set("port", PORT);
 
-console.log(io);
-
 io.on("connection", (s) => {
   const addr = s.handshake.address;
   Util.log(`[connection] total: ${s.client.conn.server.clientsCount}, from: ${addr}, id: ${s.id}`);
   s.on("disconnect", () => {
     Util.log(`[disconnect] total: ${s.client.conn.server.clientsCount}, from: ${addr}, id: ${s.id}`);
-  })
+  }) 
 })
 
 server.listen(PORT, () => {
+  http.get("http://localhost:4000/__browser_sync__?method=reload")
+    .on("error", (_) => {})
   console.log(`Listening *:${PORT}`)
 })
